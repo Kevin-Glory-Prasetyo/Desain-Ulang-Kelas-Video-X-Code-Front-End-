157ba9b1356d0816f8315ae42f90e6a9739f067d
Vicky Yohanes Putra Setiawan / 71220956 <vicky.yohanes@ti.ukdw.ac.id>
2025-09-17 14:28:38 +0700

    Connect API sementara

--- login.js
+++ login.js
@@ -11,3 +11,79 @@ toLogin.addEventListener("click", (e) => {
   e.preventDefault();
   container.classList.remove("sign-up-mode");
 });
+
+const registerBtn = document.querySelector(".sign-up button");
+
+registerBtn.addEventListener("click", async () => {
+  const firstName = document.querySelector(".sign-up input[placeholder='First Name']").value;
+  const lastName = document.querySelector(".sign-up input[placeholder='Last Name']").value;
+  const email = document.querySelector(".sign-up input[type='email']").value;
+  const password = document.querySelector(".sign-up input[type='password']").value;
+
+  try {
+    const res = await fetch("http://localhost:3000/auth/userRegister", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_first_name: firstName,
+        user_last_name: lastName,
+        user_email: email,
+        user_password: password
+      })
+    });
+
+    const data = await res.json();
+
+    if (data.statusCode === 200) {
+      alert(data.message);
+      window.location.href = "login.html";
+    } else if (data.statusCode === 400) {
+      alert(data.message);
+    } else if (data.statusCode === 409) {
+      alert(data.message);
+    } 
+
+  } catch (err) {
+    console.error(err);
+    alert("Terjadi kesalahan server.");
+  }
+});
+
+const loginBtn = document.querySelector(".sign-in button");
+
+loginBtn.addEventListener("click", async () => {
+  const email = document.querySelector(".sign-in input[type='email']").value;
+  const password = document.querySelector(".sign-in input[type='password']").value;
+
+  try {
+    const res = await fetch("http://localhost:3000/auth/userLogin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_email: email,
+        user_password: password
+      })
+    });
+
+    const data = await res.json();
+
+    if (data.statusCode === 200) {
+      alert(data.message); 
+      sessionStorage.setItem("user_email", data.email);
+      window.location.href = "filelist.html";
+    } else if (data.statusCode === 400) {
+      alert(data.message); 
+    } else if (data.statusCode === 401) {
+      alert(data.message);
+    } else if (data.statusCode === 404) {
+      alert(data.message);
+    } 
+  } catch (err) {
+    console.error(err);
+    alert("Terjadi kesalahan server.");
+  }
+});
